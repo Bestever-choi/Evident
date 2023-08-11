c5bdf2693826ee1cc70e8345478e2cddc0155386
kyoungyeon choi <kenbaxter1221@gmail.com>
2023-08-11 15:40:38 +0900

    diff

--- HierarchialDet-InitialPhase-Docker/test.py
+++ HierarchialDet-InitialPhase-Docker/test.py
@@ -15,61 +15,61 @@ import timm # PyTorch Image Models
 import torch
 import copy
 import time
-
+import random
+import os
 
 list_ids = [
-    {"height": 1316, "width": 2892, "id": 1, "file_name": "val_15.png"},
-    {"height": 1316, "width": 2942, "id": 2, "file_name": "val_38.png"},
-    {"height": 1316, "width": 2987, "id": 3, "file_name": "val_33.png"},
-    {"height": 1504, "width": 2872, "id": 4, "file_name": "val_30.png"},
-    {"height": 1316, "width": 2970, "id": 5, "file_name": "val_5.png"},
-    {"height": 1316, "width": 2860, "id": 6, "file_name": "val_21.png"},
-    {"height": 1504, "width": 2804, "id": 7, "file_name": "val_39.png"},
-    {"height": 1316, "width": 2883, "id": 8, "file_name": "val_46.png"},
-    {"height": 1316, "width": 2967, "id": 9, "file_name": "val_20.png"},
-    {"height": 1504, "width": 2872, "id": 10, "file_name": "val_3.png"},
-    {"height": 1316, "width": 2954, "id": 11, "file_name": "val_29.png"},
-    {"height": 976, "width": 1976, "id": 12, "file_name": "val_2.png"},
-    {"height": 1316, "width": 2870, "id": 13, "file_name": "val_16.png"},
-    {"height": 1316, "width": 3004, "id": 14, "file_name": "val_25.png"},
-    {"height": 1316, "width": 2745, "id": 15, "file_name": "val_24.png"},
-    {"height": 1504, "width": 2872, "id": 16, "file_name": "val_31.png"},
-    {"height": 1316, "width": 2782, "id": 17, "file_name": "val_26.png"},
-    {"height": 1316, "width": 2744, "id": 18, "file_name": "val_44.png"},
-    {"height": 1504, "width": 2872, "id": 19, "file_name": "val_27.png"},
-    {"height": 1504, "width": 2868, "id": 20, "file_name": "val_41.png"},
-    {"height": 1316, "width": 3000, "id": 21, "file_name": "val_37.png"},
-    {"height": 1316, "width": 2797, "id": 22, "file_name": "val_40.png"},
-    {"height": 1316, "width": 2930, "id": 23, "file_name": "val_6.png"},
-    {"height": 1316, "width": 3003, "id": 24, "file_name": "val_18.png"},
-    {"height": 1316, "width": 2967, "id": 25, "file_name": "val_13.png"},
-    {"height": 1316, "width": 2822, "id": 26, "file_name": "val_8.png"},
-    {"height": 1316, "width": 2836, "id": 27, "file_name": "val_49.png"},
-    {"height": 1316, "width": 2704, "id": 28, "file_name": "val_23.png"},
-    {"height": 976, "width": 1976, "id": 29, "file_name": "val_1.png"},
-    {"height": 1504, "width": 2872, "id": 30, "file_name": "val_43.png"},
-    {"height": 1504, "width": 2872, "id": 31, "file_name": "val_28.png"},
-    {"height": 1504, "width": 2872, "id": 32, "file_name": "val_19.png"},
-    {"height": 1316, "width": 2728, "id": 33, "file_name": "val_14.png"},
-    {"height": 1316, "width": 2747, "id": 34, "file_name": "val_32.png"},
-    {"height": 976, "width": 1976, "id": 35, "file_name": "val_36.png"},
-    {"height": 1316, "width": 2829, "id": 36, "file_name": "val_47.png"},
-    {"height": 1316, "width": 2846, "id": 37, "file_name": "val_48.png"},
-    {"height": 1536, "width": 3076, "id": 38, "file_name": "val_17.png"},
-    {"height": 976, "width": 1976, "id": 39, "file_name": "val_42.png"},
-    {"height": 1504, "width": 2884, "id": 40, "file_name": "val_45.png"},
-    {"height": 1316, "width": 2741, "id": 41, "file_name": "val_9.png"},
-    {"height": 1316, "width": 2794, "id": 42, "file_name": "val_4.png"},
-    {"height": 1316, "width": 2959, "id": 43, "file_name": "val_34.png"},
-    {"height": 1316, "width": 2874, "id": 44, "file_name": "val_10.png"},
-    {"height": 1316, "width": 2978, "id": 45, "file_name": "val_35.png"},
-    {"height": 1504, "width": 2884, "id": 46, "file_name": "val_11.png"},
-    {"height": 1316, "width": 2794, "id": 47, "file_name": "val_12.png"},
-    {"height": 1316, "width": 2959, "id": 48, "file_name": "val_7.png"},
-    {"height": 1316, "width": 2912, "id": 49, "file_name": "val_22.png"},
-    {"height": 1504, "width": 2872, "id": 50, "file_name": "val_0.png"},
-]
-
+                          {"height": 1316, "width": 2892, "id": 1, "file_name": "val_15.png"},
+                          {"height": 1316, "width": 2942, "id": 2, "file_name": "val_38.png"},
+                          {"height": 1316, "width": 2987, "id": 3, "file_name": "val_33.png"},
+                          {"height": 1504, "width": 2872, "id": 4, "file_name": "val_30.png"},
+                          {"height": 1316, "width": 2970, "id": 5, "file_name": "val_5.png"},
+                          {"height": 1316, "width": 2860, "id": 6, "file_name": "val_21.png"},
+                          {"height": 1504, "width": 2804, "id": 7, "file_name": "val_39.png"},
+                          {"height": 1316, "width": 2883, "id": 8, "file_name": "val_46.png"},
+                          {"height": 1316, "width": 2967, "id": 9, "file_name": "val_20.png"},
+                          {"height": 1504, "width": 2872, "id": 10, "file_name": "val_3.png"},
+                          {"height": 1316, "width": 2954, "id": 11, "file_name": "val_29.png"},
+                          {"height": 976, "width": 1976, "id": 12, "file_name": "val_2.png"},
+                          {"height": 1316, "width": 2870, "id": 13, "file_name": "val_16.png"},
+                          {"height": 1316, "width": 3004, "id": 14, "file_name": "val_25.png"},
+                          {"height": 1316, "width": 2745, "id": 15, "file_name": "val_24.png"},
+                          {"height": 1504, "width": 2872, "id": 16, "file_name": "val_31.png"},
+                          {"height": 1316, "width": 2782, "id": 17, "file_name": "val_26.png"},
+                          {"height": 1316, "width": 2744, "id": 18, "file_name": "val_44.png"},
+                          {"height": 1504, "width": 2872, "id": 19, "file_name": "val_27.png"},
+                          {"height": 1504, "width": 2868, "id": 20, "file_name": "val_41.png"},
+                          {"height": 1316, "width": 3000, "id": 21, "file_name": "val_37.png"},
+                          {"height": 1316, "width": 2797, "id": 22, "file_name": "val_40.png"},
+                          {"height": 1316, "width": 2930, "id": 23, "file_name": "val_6.png"},
+                          {"height": 1316, "width": 3003, "id": 24, "file_name": "val_18.png"},
+                          {"height": 1316, "width": 2967, "id": 25, "file_name": "val_13.png"},
+                          {"height": 1316, "width": 2822, "id": 26, "file_name": "val_8.png"},
+                          {"height": 1316, "width": 2836, "id": 27, "file_name": "val_49.png"},
+                          {"height": 1316, "width": 2704, "id": 28, "file_name": "val_23.png"},
+                          {"height": 976, "width": 1976, "id": 29, "file_name": "val_1.png"},
+                          {"height": 1504, "width": 2872, "id": 30, "file_name": "val_43.png"},
+                          {"height": 1504, "width": 2872, "id": 31, "file_name": "val_28.png"},
+                          {"height": 1504, "width": 2872, "id": 32, "file_name": "val_19.png"},
+                          {"height": 1316, "width": 2728, "id": 33, "file_name": "val_14.png"},
+                          {"height": 1316, "width": 2747, "id": 34, "file_name": "val_32.png"},
+                          {"height": 976, "width": 1976, "id": 35, "file_name": "val_36.png"},
+                          {"height": 1316, "width": 2829, "id": 36, "file_name": "val_47.png"},
+                          {"height": 1316, "width": 2846, "id": 37, "file_name": "val_48.png"},
+                          {"height": 1536, "width": 3076, "id": 38, "file_name": "val_17.png"},
+                          {"height": 976, "width": 1976, "id": 39, "file_name": "val_42.png"},
+                          {"height": 1504, "width": 2884, "id": 40, "file_name": "val_45.png"},
+                          {"height": 1316, "width": 2741, "id": 41, "file_name": "val_9.png"},
+                          {"height": 1316, "width": 2794, "id": 42, "file_name": "val_4.png"},
+                          {"height": 1316, "width": 2959, "id": 43, "file_name": "val_34.png"},
+                          {"height": 1316, "width": 2874, "id": 44, "file_name": "val_10.png"},
+                          {"height": 1316, "width": 2978, "id": 45, "file_name": "val_35.png"},
+                          {"height": 1504, "width": 2884, "id": 46, "file_name": "val_11.png"},
+                          {"height": 1316, "width": 2794, "id": 47, "file_name": "val_12.png"},
+                          {"height": 1316, "width": 2959, "id": 48, "file_name": "val_7.png"},
+                          {"height": 1316, "width": 2912, "id": 49, "file_name": "val_22.png"},
+                          {"height": 1504, "width": 2872, "id": 50, "file_name": "val_0.png"},
+                      ]
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Detectron2 demo for builtin configs")
@@ -126,7 +126,7 @@ class Hierarchialdet:
 
     def setup(self):
         args = get_parser().parse_args()
-
+        self.seed_everything(0)
         device = torch.device(f"cuda:{0}" if torch.cuda.is_available() else 'cpu')
         self.device = device
         print(device)
@@ -142,26 +142,34 @@ class Hierarchialdet:
         checkpoint_file1 = '/opt/app/configs/epoch_51.pth'
         self.model = init_detector(cfg1, checkpoint_file1, device=device)
 
+
+        config_file2 = '/opt/app/configs/dinoswin.py'
+        cfg2 = Config.fromfile(config_file2)
+        checkpoint_file2 = '/opt/app/configs/superbest14800.pth'
+        self.modeldiff = init_detector(cfg2, checkpoint_file2, device=device)
+
         self.Threshold_enum = 0.7
-        self.Threshold = 0.0
+        self.Threshold = 0.05
+        self.Thresholddino = 0.05
+
         self.CLASSES = ['11', '12', '13', '14', '15', '16', '17', '18', '21', '22', '23', '24', '25', '26', '27', '28',
                         '31',
                         '32', '33', '34', '35', '36', '37', '38', '41', '42', '43', '44', '45', '46', '47', '48']
         self.cat = ['Caries', 'Deep Caries', 'Impacted', 'Periapical Lesion']
         self.cattoid = {'Caries': 1, 'Deep Caries': 3, 'Impacted': 0, 'Periapical Lesion': 2}
         ##two step###
-        self.test_transform = A.Compose([
-            A.LongestMaxSize(max_size=224, interpolation=1),
-            A.PadIfNeeded(min_height=224, min_width=224, border_mode=0, value=(0, 0, 0)),
-            Loginverse(always_apply=True),
-
-            A.Normalize(mean=0.5, std=0.2, max_pixel_value=255.0, always_apply=True, p=1.0),
-            ToTensorV2()
-        ])
-        self.caries = self.create_model('/opt/app/configs/carieslog.pt').to(device)
-        #self.deepcaries = self.create_model('/opt/app/configs/deepcarieslog1.pt').to(device)
-        self.impacted = self.create_model('/opt/app/configs/impactedlog.pt').to(device)
-        self.peri = self.create_model('/opt/app/configs/pallog.pt').to(device)
+        # self.test_transform = A.Compose([
+        #     A.LongestMaxSize(max_size=224, interpolation=1),
+        #     A.PadIfNeeded(min_height=224, min_width=224, border_mode=0, value=(0, 0, 0)),
+        #     Loginverse(always_apply=True),
+        #
+        #     A.Normalize(mean=0.5, std=0.2, max_pixel_value=255.0, always_apply=True, p=1.0),
+        #     ToTensorV2()
+        # ])
+        # self.caries = self.create_model('/opt/app/configs/carieslog.pt').to(device)
+        # #self.deepcaries = self.create_model('/opt/app/configs/deepcarieslog1.pt').to(device)
+        # self.impacted = self.create_model('/opt/app/configs/impactedlog.pt').to(device)
+        # self.peri = self.create_model('/opt/app/configs/pallog.pt').to(device)
 
     def process(self):
         self.setup()
@@ -219,69 +227,51 @@ class Hierarchialdet:
             enumeration[str(enum)] = (x_ref, y_ref)
             enumerationscore[str(enum)] = score
 
-            output = {}
-            cat1 = int(enum / 10) - 1
-            cat2 = enum % 10 - 1
-            corners = [[float(bbox[0]), float(bbox[1]), img_id], [float(bbox[0]), float(bbox[3]), img_id],
-                       [float(bbox[2]), float(bbox[1]), img_id], [float(bbox[2]), float(bbox[3]), img_id]]
+            new_result = inference_detector(self.modeldiff, img)
+            pred = new_result.pred_instances.cpu().numpy()
+            boxes = []
+            for i, score in enumerate(pred.scores[pred.scores > self.Threshold]):
+                output = {}
+                bbox = pred.bboxes[i]
+                x, y = ((bbox[1] + bbox[3]) / 2, (bbox[0] + bbox[2]) / 2)
+                num = self.find_closest_keys(enumeration, (x, y))
 
-            bbox_r = [round(i) for i in bbox]
-            crop1 = img[bbox_r[1]:bbox_r[3], bbox_r[0]:bbox_r[2], :]
-            crop = self.test_transform(image=crop1)["image"]
-            cat3 = None
+                disease = pred.labels[i]
 
-            output['corners'] = corners
+                cat1 = int(num / 10) - 1
+                cat2 = num % 10 - 1
+                cat3 = self.cattoid[self.cat[disease - 1]]
 
-            if enum % 10 == 8:
-                psi = self.impacted(crop.to(self.device).unsqueeze(0))
-                psi = F.softmax(psi, dim=1)
-                if psi[0][0] > 0.7:
-                    cat3 = 0
-                    s = psi[0][0]
-                    output['name'] = str(cat1) + '-' + str(cat2) + '-' + str(cat3)
-                    output['probability'] = float(score * s)
-                    boxes.append(copy.deepcopy(output))
-            else:
-                ps = self.caries(crop.to(self.device).unsqueeze(0))
-                ps = F.softmax(ps, dim=1)
-                if ps[0][0] > 0.5:
-                    temp = ps[0][0]
-                    cat3 = 1
-                    s = temp
-                    output['name'] = str(cat1) + '-' + str(cat2) + '-' + str(cat3)
-                    output['probability'] = float(score * s)
-                    boxes.append(copy.deepcopy(output))
+                corners = [[float(bbox[0]), float(bbox[1]), img_id], [float(bbox[0]), float(bbox[3]), img_id],
+                           [float(bbox[2]), float(bbox[1]), img_id], [float(bbox[2]), float(bbox[3]), img_id]]
+                # [x1, y1, image_id], [x2, y2, image_id], [x3, y3, image_id], [x4, y4, image_id]
+                output['name'] = str(cat1) + '-' + str(cat2) + '-' + str(cat3)
+                output['corners'] = corners
+                output['probability'] = float(score * enumerationscore[str(num)])
+                boxes.append(output)
 
-                psp = self.peri(crop.to(self.device).unsqueeze(0))
-                psp = F.softmax(psp, dim=1)
-                if psp[0][1] > 0.5:
-                    cat3 = 2
-                    s = psp[0][1]
-                    output['name'] = str(cat1) + '-' + str(cat2) + '-' + str(cat3)
-                    output['probability'] = float(score * s)
-                    boxes.append(copy.deepcopy(output))
+            new_result = inference_detector(self.model, img)
+            pred = new_result.pred_instances.cpu().numpy()
 
-        new_result = inference_detector(self.model, img)
-        pred = new_result.pred_instances.cpu().numpy()
-        for i, score in enumerate(pred.scores[pred.scores > self.Threshold]):
-            output = {}
-            bbox = pred.bboxes[i]
-            x, y = ((bbox[1] + bbox[3]) / 2, (bbox[0] + bbox[2]) / 2)
-            num = self.find_closest_keys(enumeration, (x, y))
+            for i, score in enumerate(pred.scores[pred.scores < self.Thresholddino]):
+                output = {}
+                bbox = pred.bboxes[i]
+                x, y = ((bbox[1] + bbox[3]) / 2, (bbox[0] + bbox[2]) / 2)
+                num = self.find_closest_keys(enumeration, (x, y))
 
-            disease = pred.labels[i]
+                disease = pred.labels[i]
 
-            cat1 = int(num / 10) - 1
-            cat2 = num % 10 - 1
-            cat3 = self.cattoid[self.cat[disease - 1]]
+                cat1 = int(num / 10) - 1
+                cat2 = num % 10 - 1
+                cat3 = self.cattoid[self.cat[disease - 1]]
 
-            corners = [[float(bbox[0]), float(bbox[1]), img_id], [float(bbox[0]), float(bbox[3]), img_id],
-                       [float(bbox[2]), float(bbox[1]), img_id], [float(bbox[2]), float(bbox[3]), img_id]]
-            # [x1, y1, image_id], [x2, y2, image_id], [x3, y3, image_id], [x4, y4, image_id]
-            output['name'] = str(cat1) + '-' + str(cat2) + '-' + str(cat3)
-            output['corners'] = corners
-            output['probability'] = float(score * enumerationscore[str(num)])
-            boxes.append(output)
+                corners = [[float(bbox[0]), float(bbox[1]), img_id], [float(bbox[0]), float(bbox[3]), img_id],
+                           [float(bbox[2]), float(bbox[1]), img_id], [float(bbox[2]), float(bbox[3]), img_id]]
+                # [x1, y1, image_id], [x2, y2, image_id], [x3, y3, image_id], [x4, y4, image_id]
+                output['name'] = str(cat1) + '-' + str(cat2) + '-' + str(cat3)
+                output['corners'] = corners
+                output['probability'] = float(score * enumerationscore[str(num)])
+                boxes.append(output)
 
         return boxes
 
@@ -312,6 +302,16 @@ class Hierarchialdet:
         cariesmodel.eval()
 
         return cariesmodel
+
+    def seed_everything(self, seed):
+        random.seed(seed)
+        os.environ['PYTHONHASHSEED'] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
+
 
 if __name__ == "__main__":
     Hierarchialdet().process()
