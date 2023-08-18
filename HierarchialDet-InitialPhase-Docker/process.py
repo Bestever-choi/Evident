8f88a24442ace3d5592878942ac604a7694ea7f6
kyoungyeon choi <kenbaxter1221@gmail.com>
2023-08-18 21:44:11 +0900

    0.01

--- HierarchialDet-InitialPhase-Docker/process.py
+++ HierarchialDet-InitialPhase-Docker/process.py
@@ -1599,7 +1599,7 @@ class Hierarchialdet:
         self.modeldiff = init_detector(cfg2, checkpoint_file2, device=device)
 
         self.Threshold_enum = 0.7
-        self.Threshold = 0.05
+        self.Threshold = 0.01
         self.CLASSES = ['11', '12', '13', '14', '15', '16', '17', '18', '21', '22', '23', '24', '25', '26', '27', '28',
                         '31',
                         '32', '33', '34', '35', '36', '37', '38', '41', '42', '43', '44', '45', '46', '47', '48']
